d897f871cb2187f2c472ba8c28199c28bbad0b82
Jacob Vibert <jvt05@ukc02yc43tjg5h.bskyb.com>
2020-02-13 14:37:58 +0000

    static test

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { render } from "@testing-library/react";
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "../App";
 
 describe("Add Task Button", () => {
-  it("Renders without crashing", () => {
+  it("Creates a task when clicking the Add Button", () => {
     const AppPage = render(<App />);
-    AppPage.getByText("Add");
+    expect(AppPage.queryByText("Task 1")).toBe(null);
+    fireEvent.change(AppPage.getByPlaceholderText("Name your Task here..."), {
+      target: { value: "Task 1" }
+    });
+    fireEvent.click(AppPage.getByText("Add"));
+    expect(AppPage.queryByText("Task 1")).not.toBe(null);
   });
 });
