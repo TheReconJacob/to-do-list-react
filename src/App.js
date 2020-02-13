27aa328b1af0b51426b61657914784f936586088
Jacob Vibert <jvt05@ukc02yc43tjg5h.bskyb.com>
2020-02-13 11:21:10 +0000

    eh

--- src/App.js
+++ src/App.js
@@ -1,151 +1,138 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const [list, setList] = useState([]);
 
-    this.state = {
-      newTask: "",
-      list: []
-    };
-  }
-  updateInput(id, value) {
-    //update react state
-    this.setState({
-      [id]: value
-    });
-  }
+  const addItem = item => {
+    let listClone = [...list];
+    listClone.push({ title: item, completed: false });
+    setList(listClone);
+  };
 
-  addTask() {
-    //create task with unique ID
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-    const newTask = {
-      id: 1 * getRandomInt(99999),
-      value: this.state.newTask.slice(),
-      but1: 1 * getRandomInt(99999),
-      but2: 1 * getRandomInt(99999)
-    };
+  const setCompleted = itemIndex => {
+    let listClone = [...list];
+    listClone[itemIndex].completed = true;
+    setList(listClone);
+  };
 
-    //copy of current list of tasks
-    const list = [...this.state.list];
+  let listDOM = [];
+  list.forEach((item, index) => {
+    listDOM.push(
+      <li key={index}>
+        {item.title}{" "}
+        <button onClick={event => setCompleted(index)}>done</button>
+      </li>
+    );
+  });
 
-    //add new task to list
-    list.push(newTask);
-
-    console.log(list);
-    //update state with new list and reset newTask input
-    this.setState({
-      list,
-      newTask: ""
-    });
-  }
-
-  doneTask(taskID, taskBut1, taskBut2) {
-    document.getElementById(taskID).style.textDecoration = "line-through";
-    document.getElementById(taskID).className = "Done";
-    document.getElementById(taskBut1).outerHTML = "";
-    document.getElementById(taskBut2).style = "";
-  }
-  deleteTask(taskID) {
-    document.getElementById(taskID).outerHTML = "";
-  }
-  filterTask() {
-    let classes = document.getElementsByClassName("Done");
-
-    Array.prototype.forEach.call(classes, function(Class) {
-      Class.style.display = "none";
-    });
-  }
-  filterTask() {
-    document.getElementById("filter").style.display = "none";
-    document.getElementById("unfilter").style = "";
-    let classes = document.getElementsByClassName("Done");
-    Array.prototype.forEach.call(classes, function(Class) {
-      Class.style.display = "none";
-    });
-  }
-  unfilterTask() {
-    document.getElementById("unfilter").style.display = "none";
-    document.getElementById("filter").style.display = "inline";
-    let classes = document.getElementsByClassName("Done");
-    Array.prototype.forEach.call(classes, function(Class) {
-      Class.style.display = "block";
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div>
-          Add an Task...
-          <br />
+  return (
+    <div className="App">
+      <div>
+        Add an Task...
+        <br />
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            let data = new FormData(event.target);
+            addItem(data.get("taskTitle"));
+          }}
+        >
           <input
             type="text"
             class="type-task"
             placeholder="Name your Task here..."
-            value={this.state.newTask}
-            onChange={newTask =>
-              this.updateInput("newTask", newTask.target.value)
-            }
+            name="taskTitle"
           />
-          <button class="add-task" onClick={() => this.addTask()}>
-            Add
-          </button>
-          <br />
-          <button
-            id="filter"
-            class="active-tasks"
-            onClick={() => this.filterTask()}
-          >
-            Show Active Tasks Only
-          </button>
-          <button
-            id="unfilter"
-            class="all-tasks"
-            style={{
-              display: "none"
-            }}
-            onClick={() => this.unfilterTask()}
-          >
-            Show All Tasks
-          </button>
-          <br />
-        </div>
+          <button class="add-task">Add</button>
+        </form>
         <br />
-        <div>
-          Tasks:
-          <ul>
-            {this.state.list.map(task => {
-              return (
-                <li class="task" id={task.id} key={task.id}>
-                  {task.value}
-
-                  <button
-                    id={task.but1}
-                    onClick={() => this.doneTask(task.id, task.but1, task.but2)}
-                  >
-                    done
-                  </button>
-                  <button
-                    id={task.but2}
-                    style={{
-                      visibility: "hidden"
-                    }}
-                    onClick={() => this.deleteTask(task.id)}
-                  >
-                    delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <button id="filter" class="active-tasks">
+          Show Active Tasks Only
+        </button>
+        <button
+          id="unfilter"
+          class="all-tasks"
+          style={{
+            display: "none"
+          }}
+        >
+          Show All Tasks
+        </button>
+        <br />
       </div>
-    );
-  }
+      <br />
+      <div>
+        Tasks:
+        <ul>{listDOM}</ul>
+      </div>
+    </div>
+  );
 }
+// updateInput(id, value) {
+//   //update react state
+//   this.setState({
+//     [id]: value
+//   });
+// }
+
+// addTask() {
+//   //create task with unique ID
+//   function getRandomInt(max) {
+//     return Math.floor(Math.random() * Math.floor(max));
+//   }
+//   const newTask = {
+//     id: 1 * getRandomInt(99999),
+//     value: this.state.newTask.slice(),
+//     but1: 1 * getRandomInt(99999),
+//     but2: 1 * getRandomInt(99999)
+//   };
+
+//   //copy of current list of tasks
+//   const list = [...this.state.list];
+
+//   //add new task to list
+//   list.push(newTask);
+
+//   console.log(list);
+//   //update state with new list and reset newTask input
+//   this.setState({
+//     list,
+//     newTask: ""
+//   });
+// }
+
+// doneTask(taskID, taskBut1, taskBut2) {
+//   document.getElementById(taskID).style.textDecoration = "line-through";
+//   document.getElementById(taskID).className = "Done";
+//   document.getElementById(taskBut1).outerHTML = "";
+//   document.getElementById(taskBut2).style = "";
+// }
+// deleteTask(taskID) {
+//   document.getElementById(taskID).outerHTML = "";
+// }
+// filterTask() {
+//   let classes = document.getElementsByClassName("Done");
+
+//   Array.prototype.forEach.call(classes, function(Class) {
+//     Class.style.display = "none";
+//   });
+// }
+// filterTask() {
+//   document.getElementById("filter").style.display = "none";
+//   document.getElementById("unfilter").style = "";
+//   let classes = document.getElementsByClassName("Done");
+//   Array.prototype.forEach.call(classes, function(Class) {
+//     Class.style.display = "none";
+//   });
+// }
+// unfilterTask() {
+//   document.getElementById("unfilter").style.display = "none";
+//   document.getElementById("filter").style.display = "inline";
+//   let classes = document.getElementsByClassName("Done");
+//   Array.prototype.forEach.call(classes, function(Class) {
+//     Class.style.display = "block";
+//   });
+// }
 
 export default App;
---
+++ src/__test__/App.test.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+describe("Add Task Button", () => {
+  it("Renders without crashing", () => {
+    const AppPage = render(<App />);
+    AppPage.getByText("add");
+  });
+});
